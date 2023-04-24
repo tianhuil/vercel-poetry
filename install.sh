740350a7dd56ffeba6ccd558d9101e7acee27e16
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-04-24 10:18:07 -0400

    Diagnostic printing

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "Using Poetry Install"
 # export POETRY_HOME=/opt/poetry
 pip install poetry
 poetry install
+pip show poetry
 
 # $POETRY_HOME/bin/poetry install
 echo "Finished Installing Poetry"
