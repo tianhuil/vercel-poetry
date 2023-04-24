bfe9d7f2f9403dda85646a5f9b2480eb1f925d25
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-04-24 10:22:42 -0400

    Get install location for poetry

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@ echo "========================================================"
 echo "Using Poetry Install"
 # export POETRY_HOME=/opt/poetry
 pip install poetry
-poetry install
 echo "============================="
 echo "Printing pip location"
 pip show poetry
 pip list -v 
+
+echo "============================="
+poetry install
+ls ~/.config/pypoetry
+echo $POETRY_CONFIG_DIR
 
 # $POETRY_HOME/bin/poetry install
 echo "Finished Installing Poetry"
