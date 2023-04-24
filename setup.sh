e5886245dec0e9f843160bb8feaf435240467646
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-04-24 09:49:29 -0400

    Update script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-echo "============================"
+echo "========================================================"
 echo "Using Poetry Install"
 export POETRY_HOME=/opt/poetry
 curl -sSL https://install.python-poetry.org | python3 -
@@ -8,5 +8,6 @@ echo "Poetry Version"
 $POETRY_HOME/bin/poetry --version
 
 $POETRY_HOME/bin/poetry install
+mkdir public
 echo "Finished Installing Poetry"
-echo "============================"
+echo "========================================================"
