c23c8daae60d4a72524a4fef0428d7a97957147d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-04-24 10:19:27 -0400

    Update echo

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ echo "Using Poetry Install"
 # export POETRY_HOME=/opt/poetry
 pip install poetry
 poetry install
+echo "============================="
+echo "Printing pip location"
 pip show poetry
+pip list -v 
 
 # $POETRY_HOME/bin/poetry install
 echo "Finished Installing Poetry"
