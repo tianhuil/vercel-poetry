e8a04502fea767722060f696154fd6c7aca20919
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-04-24 09:29:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+echo "Using Poetry"
 export POETRY_HOME=/opt/poetry
 curl -sSL https://install.python-poetry.org | python3 -
+echo "Poetry Version"
 $POETRY_HOME/bin/poetry --version
 
 poetry install
